7179174d0cb91f2ce4d69e30ecc93dc279563f97
Rostyslav Bulyga <goudini@gmail.com>
2024-11-24 21:51:24 +0200

    Code quality

--- outages/__init__.py
+++ outages/__init__.py
@@ -1,5 +1,8 @@
+from .outage import Outage
 from .outages_notifier import OutagesNotifier
 from .outages_reader import OutagesReader
+
+__all__ = ["Outage", "OutagesNotifier", "OutagesReader"]
 
 outages_reader = OutagesReader()
 outages_notifier = OutagesNotifier()
--- users/__init__.py
+++ users/__init__.py
@@ -1,3 +1,6 @@
+from .user import User
 from .users_storage import UsersStorage
+
+__all__ = ["User", "UsersStorage"]
 
 users = UsersStorage()
